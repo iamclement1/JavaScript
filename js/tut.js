0f545c23544e059eb8e2d272d28b01e767740aaa
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-01-27 00:44:30 +0100

    for loop

--- js/tut.js
+++ js/tut.js
@@ -181,3 +181,19 @@ if ((flavor === 'vanilla' || flavor === 'strawberry')&&(vessel ==='cone' || vess
 // }while(yourName){
 //     console.log(yourName)
 // }
+
+for(let num = 0; num <= 12; num = num + 2){
+    console.log(num);
+}
+
+// let result = 1;
+// for (let counter = 0; counter < 10; counter = counter + 1)  {
+//     result = result * 2;
+// } console.log(result);
+
+for (let current = 20; ; current = current + 1 ){
+    if(current % 7 === 0) {
+        console.log(current);
+        break;
+    }
+}
